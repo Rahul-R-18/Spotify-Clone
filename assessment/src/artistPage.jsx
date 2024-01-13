da739878cbff70c6e6594437b7a2095c242f5e3f
Rahul-R-24 <rahulram2204@gmail.com>
2024-01-13 02:37:27 -0500

    v1

--- assessment/src/artistPage.jsx
+++ assessment/src/artistPage.jsx
@@ -1,12 +1,26 @@
 import './artistPage.css'
+import React from 'react';
 
-/*
-This component is intended to create a page for each artist.
-You can use this component if you want, but it is not required.
-*/
+const ArtistPage = ({ artist }) => {
+    return (
+        <div className="artistPage">
+            <div className="infoBox">
+                <h2>{artist.name}</h2>
+                <img src={artist.image_src} alt={artist.name} />
+                <b> <p className = "minutes">Minutes Listened: </p>
+                <p className = "num"> {artist.minutes} </p> </b>
+            </div>
+            <div className="songsBox">
+                <h2 className = "header">Top Songs</h2>
+                <ol> <b>
+                    {artist.songs.map((song, index) => (
+                        <li key={index}>{song}</li>
+                    ))} </b>
+                </ol> 
+            </div>
+        </div>
+    );
+};
 
-const ArtistPage = (props) => {
-   
-}
+export default ArtistPage;
 
-export default ArtistPage
--- assessment/src/spotify.jsx
+++ assessment/src/spotify.jsx
@@ -1,13 +1,40 @@
 import artists from './artists.jsx'
 import './spotify.css'
+import React, { useState } from 'react';
+import ArtistPage from './artistPage';
 /* 
 This is the component for the entire page. You can make additional components as you please.
 Feel free to edit anything in this file except the name of the component.
-*/
+
 const Spotify = (props) => {
     return (
         <div>Spotify</div>
     )
 }
 
-export default Spotify
+export default Spotify */
+
+
+
+const Spotify = () => {
+    const [currIndex, setIndex] = useState(0);
+    const length = artists.length;
+
+    const handleNext = () => {
+        setIndex((currIndex + 1) % length);
+    };
+
+    const handlePrevious = () => {
+        setIndex((currIndex - 1 + length) % length);
+    };
+
+    return (
+        <div className="spotify">
+            <button class = "left-triangle" onClick={handlePrevious}></button>
+            <ArtistPage artist={artists[currIndex]} />
+            <button class = "right-triangle" onClick={handleNext}></button>
+        </div>
+    );
+};
+
+export default Spotify;
